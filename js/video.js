5e312a96859bb6388481c3370a3bb52c769afd0f
lingyuz <71449690+lingyuz@users.noreply.github.com>
2021-04-01 22:48:50 -0400

    Update3

--- js/video.js
+++ js/video.js
@@ -2,7 +2,7 @@ var video = document.getElementById("player1");;
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
-
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
 });
 
 document.querySelector("#play").addEventListener("click", function() {
